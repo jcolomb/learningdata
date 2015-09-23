4400979d5ce95e9cc897b5ac06bd9900a228cc61
colombj <julien.colomb@fu-berlin.de>
2015-09-23 09:13:12 +0200

    masteradded

---
+++ flightdata/analysis_R/Mastercode.r
@@ -0,0 +1,13 @@
+setwd("~/Gits/learningdata/flightdata/analysis_R/")
+#install.packages(c("plotrix", "MASS","vioplot", "UsingR","gplots","lattice","gdata","rfigshare"))
+
+source("getdatafigshare.r")
+#metafile$shortname
+
+## need to put shortnames on figshare and get it back from there
+## empty getdata.r, which is called in each sub-analysis
+
+#IDshortname= fs_create (title="Names of the flies used in self-learning exp.",description="used to make good figures", type=("dataset"))
+#fs_upload(IDshortname,"shortnames2.csv")
+
+source("graph/doallanalysis.r")
