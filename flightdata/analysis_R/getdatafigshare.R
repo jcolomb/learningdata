98c2731ef2348ee48eb01e6e3a3520f20c1d89c2
colombj <julien.colomb@fu-berlin.de>
2015-09-25 09:31:23 +0200

    debug start knitr it.

--- flightdata/analysis_R/getdatafigshare.R
+++ flightdata/analysis_R/getdatafigshare.R
@@ -1,9 +1,9 @@
 require(rfigshare) 
 
-options(FigshareKey = "jBkrLNsYbeov2oM09cXBBw")
-options(FigsharePrivateKey = "LhQkDeJaVJzAhWRIKYeobA")
-options(FigshareToken = "BYuYn4OjWjd8njBKCyeFXQOEAYY1MfLJ1Y0z80rVWj6AXYuYn4OjWjd8njXKCyeFXQ")
-options(FigsharePrivateToken = "aC2q4lBod3Xl52CeKwp7Fg")
+#options(FigshareKey = "jBkrLNsYbeov2oM09cXBBw")
+#options(FigsharePrivateKey = "LhQkDeJaVJzAhWRIKYeobA")
+#options(FigshareToken = "BYuYn4OjWjd8njBKCyeFXQOEAYY1MfLJ1Y0z80rVWj6AXYuYn4OjWjd8njXKCyeFXQ")
+#options(FigsharePrivateToken = "aC2q4lBod3Xl52CeKwp7Fg")
 
 fs_auth()
 metafileurl= fs_download (695950)
@@ -15,3 +15,27 @@ metafile$laser.power=suppressWarnings(as.numeric (as.character(metafile$laser.po
 ##get data with remarks out
 
 metafile=subset (metafile, is.na(metafile$remarks))
+
+Namesurl= fs_download(1552031)
+Names= read.csv (Namesurl, header=TRUE,)[,-1]
+
+Names[,3]=as.factor(gsub("RL","\n",Names[,2]))
+Names[,4]=as.factor(gsub("RL","",Names[,2]))
+levels(Names[,1])= c(levels(Names[,1]),levels(metafile$genotype))
+metafile$shortname=metafile$genotype
+metafile$shortnamelegend = metafile$genotype
+levels(metafile$shortname)= c(levels(metafile$shortname), levels(Names[,3]))
+for (i in 1: length(levels(metafile$genotype))){
+#print(i)
+metafile$shortname[metafile$genotype==levels(metafile$genotype)[i]]= as.character(Names[Names[,1]==levels(metafile$genotype)[i],3])
+}
+metafile$shortname=droplevels(metafile$shortname)
+metafile$shortnamelegend =droplevels(metafile$shortnamelegend)
+
+levels(metafile$shortnamelegend)= c(levels(metafile$shortnamelegend), levels(Names[,4]))
+for (i in 1: length(levels(metafile$genotype))){
+#print(i)
+metafile$shortnamelegend[metafile$genotype==levels(metafile$genotype)[i]]= as.character(Names[Names[,1]==levels(metafile$genotype)[i],4])
+}
+
+Names= read.csv (Namesurl, header=TRUE,)[,-1]
