e4eac74e6d0f66a2b56045f662c9f62d6df91149
colombj <julien.colomb@fu-berlin.de>
2015-09-29 22:43:19 +0200

    cleaning up

--- flightdata/analysis_R/changegenotnames.R
+++
@@ -1,9 +0,0 @@
-changename <- function(x,metafile){
-	print(levels(metafile$genotype)[x])
-	cat("give a new name")
-newname= readLines(n=1)
-
-levels(metafile$genotype)=c(levels(metafile$genotype),newname)
-metafile$genotype[metafile$genotype==levels(metafile$genotype)[x]]=newname
-
-}
